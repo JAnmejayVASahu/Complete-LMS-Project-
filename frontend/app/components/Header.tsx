685f449d83eb43ea6ec0f2dbcd9983f3ae748532
Janmejay Sahu <chikunsahu73@gmail.com>
2024-08-11 11:40:22 +0530

    Adding logo in Header

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import React, { FC, useState } from "react";
 
 type Props = {
@@ -20,7 +21,7 @@ const Header: FC<Proops> = (props) => {
       }
     });
   }
-  
+
   return (
     <div className="w-full relative">
       <div
@@ -29,7 +30,21 @@ const Header: FC<Proops> = (props) => {
             ? "dark:bg-opacity-50 : dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#fff] shadow-xl transition duration-500"
             : "w-full border-b dark:border-[#fff1c] h-[80px] z-[80] dark:shadow"
         }`}
-      ></div>
+      >
+        <div className="w-[95%] 800:w-[92%] m-auto py-2 h-full">
+          <div className="w-full h-[80] flex items-center justify-between p-3">
+            <Link
+              href={"/"}
+              className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white blog-title font-[Poppins] hover:scale-110 transition duration-300 italic"
+            >
+              <span className=" animate-typing px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white font-bold italic">
+                Sticky
+              </span>
+              Learning
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
