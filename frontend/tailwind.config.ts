1750f0d19921da47c948d2ac763a0bbee5756ea5
Janmejay Sahu <chikunsahu73@gmail.com>
2024-08-11 10:20:01 +0530

    Completed of file structure and starting of header

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,3 +1,4 @@
+import { Josefin_Sans, Poppins } from "next/font/google";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -6,12 +7,26 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: ["class"],
   theme: {
     extend: {
+      fontFamily: {
+        Poppins: ["var(--font-Poppins)"],
+        Josefin_Sans: ["var(--font-Josefin_Sans)"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      screens: {
+        "1000": "1000px",
+        "1100": "1100px",
+        "1200": "1200px",
+        "1300": "1300px",
+        "1500": "1500px",
+        "800px": "800px",
+        "400px": "400px",
       },
     },
   },
