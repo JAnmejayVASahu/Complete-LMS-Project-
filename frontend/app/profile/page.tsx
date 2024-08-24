24bbbc38c2630c71ee03aa52d186fc61bed108a4
Janmejay Sahu <chikunsahu73@gmail.com>
2024-08-24 19:59:30 +0530

    when click on profile icon it direct to profile page

---
+++ frontend/app/profile/page.tsx
@@ -0,0 +1,36 @@
+'use client'
+import { FC, useState } from "react";
+import { useSelector } from "react-redux";
+import Protected from "../hooks/useProtected";
+import Heading from "../utils/heading";
+import Header from "../components/Header";
+
+type Props = {};
+
+const Page: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(5);
+  const [route, setRoute] = useState("Login");
+  const {user} = useSelector((state:any) => state.auth);
+
+  return (
+    <div className="min-h-screen">
+      <Protected>
+        <Heading
+          title={`${user?.name} profile - Elearning`}
+          description="Sticky Learning is a platform for students to learn and get help from teachers"
+          keywords="Prograaming,MERN,Redux,Machine Learning"
+        />
+        <Header
+          open={open}
+          setOpen={setOpen}
+          activeItem={activeItem}
+          setRoute={setRoute}
+          route={route}
+        />
+      </Protected>
+    </div>
+  );
+};
+
+export default Page;
